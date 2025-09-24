aa0aad59fa39ea64a9d79d79588dd6286c484241
Alan Liang <alan.liang@arm.com>
2025-09-24 18:36:46 +0100

    Move sanitizer flags to build script, add MSVC sanitizers
    Signed-off-by: Alan Liang <alan.liang@arm.com>
    Change-Id: I5d4162a0908da41a14631e27a3bd83d34ff2d517

--- cmake/toolchain/gnu_compiler_options.cmake
+++ cmake/toolchain/gnu_compiler_options.cmake
@@ -5,10 +5,3 @@
 
 # Set warnings as errors and enable all warnings
 set(VMEL_COMPILE_OPTIONS -Werror -Wall -Wextra -Wsign-conversion -Wpedantic -fno-strict-aliasing) # FIXME: readd -Wconversion
-
-if(NOT "${VMEL_GCC_SANITIZERS}" STREQUAL "")
-    message(STATUS "GCC Sanitizer enabled: ${VMEL_GCC_SANITIZERS}")
-    add_compile_options(-fsanitize=${VMEL_GCC_SANITIZERS})
-    add_compile_options(-fno-sanitize=alignment -fno-sanitize-recover=all)
-    add_link_options(-fsanitize=${VMEL_GCC_SANITIZERS})
-endif()
