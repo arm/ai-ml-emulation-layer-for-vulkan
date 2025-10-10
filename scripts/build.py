0f0fe7e5eb7e59bc51c91fae7f68c3b28101cf91
Mohammadreza Ameri Mahabadian <mohammadreza.amerimahabadian@arm.com>
2025-10-10 12:08:36 +0100

    Fix for running MSVC address sanitizer (#108)
    Change-Id: If616b595719e6952e37c98cba68722039e59670f
    
    Signed-off-by: Mohammadreza Ameri Mahabadian <mohammadreza.amerimahabadian@arm.com>

--- scripts/build.py
+++ scripts/build.py
@@ -157,8 +157,10 @@ class Builder:
                     "-DCMAKE_EXE_LINKER_FLAGS=-fsanitize=undefined,address"
                 )
             elif system == "Windows":
-                cmake_setup_cmd.append("-DCMAKE_CXX_FLAGS=/Zi /RTC1 /GS")
-                cmake_setup_cmd.append("-DCMAKE_EXE_LINKER_FLAGS=/GS")
+                cmake_setup_cmd.append("-DCMAKE_CXX_FLAGS=/fsanitize=address /Zi /MDd")
+                cmake_setup_cmd.append(
+                    "-DCMAKE_EXE_LINKER_FLAGS=/INFERASANLIBS /DEBUG /INCREMENTAL:NO"
+                )
             else:
                 print(f"ERROR: sanitizer is not supported on system: {system}")
 
