3f203ee97da94e350f06d52589a120acf2c1132e
Davide Grohmann <davide.grohmann@arm.com>
2025-10-21 16:26:46 +0200

    Improve sin accuracy by using Taylor expansion
    Use Taylor for small values (<1e-3) where: sin(diff) ~= diff.
    
    Add a SIN test to reproduce the TOSA tolerance issue
    
    Also make sure that the glsl compilation command fails on errors.
    
    Change-Id: I9711c31755a437bd801a9654c04f4622683788cd
    Signed-off-by: Davide Grohmann <davide.grohmann@arm.com>

--- graph/compute.cpp
+++ graph/compute.cpp
@@ -2870,8 +2870,8 @@ void GraphPipeline::makeSigmoid(const std::shared_ptr<TensorDescriptor> &input,
 
 void GraphPipeline::makeSin(const std::shared_ptr<TensorDescriptor> &input1,
                             const std::shared_ptr<TensorDescriptor> &output, const std::string &debugName) {
-    auto pipeline =
-        std::make_shared<ElementwiseUnary>(loader, device, pipelineCache, input1, output, debugName, "sin(value1)");
+    auto pipeline = std::make_shared<ElementwiseUnary>(loader, device, pipelineCache, input1, output, debugName,
+                                                       "sin_hybrid(value1)");
     pipelines.emplace_back(pipeline);
 }
 
