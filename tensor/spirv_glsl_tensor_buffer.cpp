7923f3431539df7805a6ce1b4f29298c22a706d0
Davide Grohmann <davide.grohmann@arm.com>
2025-10-21 16:18:20 +0200

    Update SBOM files for new dependencies
    Small code change to align to new version of dependency
    
    Change-Id: Icd3afd9b5ee9e5e099a38d2cf7a9d191679ab49b
    Signed-off-by: Davide Grohmann <davide.grohmann@arm.com>

--- tensor/spirv_glsl_tensor_buffer.cpp
+++ tensor/spirv_glsl_tensor_buffer.cpp
@@ -114,7 +114,7 @@ CompilerTensorAsBuffer::CompilerTensorAsBuffer(std::vector<uint32_t> spirv_) : C
     std::map<uint32_t, uint32_t> typeMap{};
     for (auto tensorTypeId : tensorTypeIds) {
         // Get tensor element type id
-        uint32_t elementTypeId = get<SPIRType>(tensorTypeId).tensor.type;
+        uint32_t elementTypeId = get<SPIRType>(tensorTypeId).ext.tensor.type;
 
         if (auto tensorStructDoneId = typeMap[elementTypeId]; tensorStructDoneId != 0) {
             auto &tensorStructDone = get<SPIRType>(tensorStructDoneId);
@@ -337,7 +337,7 @@ uint32_t CompilerTensorAsBuffer::createTensorBDA(uint32_t tensorTypeId) {
 
     // Create buffer array type
     // Same as spirv_parse.cpp:parse `case OpTypeRuntimeArray`
-    uint32_t elementTypeId = tensorType.tensor.type;
+    uint32_t elementTypeId = tensorType.ext.tensor.type;
     auto *elementType = &get<SPIRType>(elementTypeId);
     if (elementType->basetype == SPIRType::Boolean) {
         // Bool tensors are strictly defined to use 8-bit bools, but the same is not true for buffers.
