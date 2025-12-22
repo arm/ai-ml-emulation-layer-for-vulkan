8a1018501f37ad654c3a3984e2a39411405739a7
Freddie Liardet <frederick.liardet@arm.com>
2025-12-22 09:32:51 +0000

    Update pip package help message for Darwin
    Change-Id: I71572ec7e17be625a7a68cfab6fa7c72b7cce20d
    Signed-off-by: Freddie Liardet <frederick.liardet@arm.com>

--- pip_package/emulation_layer/cli.py
+++ pip_package/emulation_layer/cli.py
@@ -6,18 +6,15 @@ import os
 import sys
 
 
-def get_linux_deploy_paths():
+def get_deploy_paths():
     base = os.path.join(os.path.dirname(__file__), "deploy")
     return {
-        "LD_LIBRARY_PATH": os.path.join(base, "lib"),
-        "VK_ADD_LAYER_PATH": os.path.join(base, "share", "vulkan", "explicit_layer.d"),
-    }
-
-
-def get_windows_deploy_paths():
-    base = os.path.join(os.path.dirname(__file__), "deploy")
-    return {
-        "VK_LAYER_PATH": os.path.join(base, "bin"),
+        "DYLD_LIBRARY_PATH": os.path.join(base, "lib"),  # Darwin
+        "LD_LIBRARY_PATH": os.path.join(base, "lib"),  # Linux
+        "VK_LAYER_PATH_windows": os.path.join(base, "bin"),  # Windows
+        "VK_LAYER_PATH": os.path.join(
+            base, "share", "vulkan", "explicit_layer.d"
+        ),  # Linux, Darwin
     }
 
 
@@ -25,16 +22,23 @@ def main():
     print(
         "To use the ML SDK for Vulkan Emulation Layer, set these environment variables:"
     )
+    paths = get_deploy_paths()
     if sys.platform.startswith("linux"):
-        paths = get_linux_deploy_paths()
         print(f"export LD_LIBRARY_PATH={paths['LD_LIBRARY_PATH']}:$LD_LIBRARY_PATH")
-        print(f"export VK_ADD_LAYER_PATH={paths['VK_ADD_LAYER_PATH']}")
+        print(f"export VK_LAYER_PATH={paths['VK_ADD_LAYER_PATH']}:$VK_LAYER_PATH")
         print(
             "export VK_INSTANCE_LAYERS=VK_LAYER_ML_Graph_Emulation:VK_LAYER_ML_Tensor_Emulation"
         )
     elif sys.platform.startswith("win"):
-        paths = get_windows_deploy_paths()
-        print(f"$env:VK_LAYER_PATH={paths['VK_LAYER_PATH']}")
+        print(f"$env:VK_LAYER_PATH={paths['VK_LAYER_PATH_windows']};$VK_LAYER_PATH")
+        print(
+            '$env:VK_INSTANCE_LAYERS="VK_LAYER_ML_Graph_Emulation;VK_LAYER_ML_Tensor_Emulation"'
+        )
+    elif sys.platform.startswith("darwin"):
+        print(
+            f"export DYLD_LIBRARY_PATH={paths['DYLD_LIBRARY_PATH']}:$DYLD_LIBRARY_PATH"
+        )
+        print(f"$env:VK_LAYER_PATH={paths['VK_LAYER_PATH']}:$VK_LAYER_PATH")
         print(
             '$env:VK_INSTANCE_LAYERS="VK_LAYER_ML_Graph_Emulation:VK_LAYER_ML_Tensor_Emulation"'
         )
