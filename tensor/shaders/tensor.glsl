32881e0f55a0ce0037c8f1068419532b5ba69620
Ida Levison <ida.levison@arm.com>
2026-04-23 16:44:22 +0200

    Molten vk support: Use tensor buffer id explicitly instead of deriving
    Change-Id: I613c001eda07b314232d35184d8f0751bd54efbf
    Signed-off-by: Ida Levison <ida.levison@arm.com>

--- tensor/shaders/tensor.glsl
+++ tensor/shaders/tensor.glsl
@@ -1,6 +1,6 @@
 R"(
 /*
- * SPDX-FileCopyrightText: Copyright 2023-2025 Arm Limited and/or its affiliates <open-source-office@arm.com>
+ * SPDX-FileCopyrightText: Copyright 2023-2026 Arm Limited and/or its affiliates <open-source-office@arm.com>
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -23,7 +23,7 @@ const int g_TensorOperandsOutOfBoundsValue = 0x2;
 
 #define tensorSizeARM(tensor, dimension) uint(tensor.shape[dimension])
 
-#define _emu_GL_ARM_tensors_read_array(tensor, coords, value, operands, outOfBoundsValue, TYPE) {                      \
+#define _emu_GL_ARM_tensors_read_array(tensor, tensorData, coords, value, operands, outOfBoundsValue, TYPE) {          \
   int64_t _emu_GL_ARM_tensors_offset = 0;                                                                              \
   bool _emu_GL_ARM_tensors_outOfBounds = false;                                                                        \
   for (int _emu_GL_ARM_tensors_i = 0; _emu_GL_ARM_tensors_i < coords.length(); ++_emu_GL_ARM_tensors_i) {              \
@@ -44,18 +44,18 @@ const int g_TensorOperandsOutOfBoundsValue = 0x2;
       value[_emu_GL_ARM_tensors_i] = TYPE(outOfBoundsValue);                                                           \
     } else {                                                                                                           \
       value[_emu_GL_ARM_tensors_i] =                                                                                   \
-          TYPE(tensor.address.data[uint(_emu_GL_ARM_tensors_offset + _emu_GL_ARM_tensors_i)]);                         \
+          TYPE(tensorData.data[uint(_emu_GL_ARM_tensors_offset + _emu_GL_ARM_tensors_i)]);                             \
     }                                                                                                                  \
   }                                                                                                                    \
 }
 
-#define _emu_GL_ARM_tensors_read_scalar(tensor, coords, value, operands, outOfBoundsValue, TYPE) {                     \
+#define _emu_GL_ARM_tensors_read_scalar(tensor, tensorData, coords, value, operands, outOfBoundsValue, TYPE) {         \
   TYPE _emu_GL_ARM_tensors_valueArr[1];                                                                                \
-  _emu_GL_ARM_tensors_read_array(tensor, coords, _emu_GL_ARM_tensors_valueArr, operands, outOfBoundsValue, TYPE);      \
+  _emu_GL_ARM_tensors_read_array(tensor, tensorData, coords, _emu_GL_ARM_tensors_valueArr, operands, outOfBoundsValue, TYPE);      \
   value = _emu_GL_ARM_tensors_valueArr[0];                                                                             \
 }
 
-#define _emu_GL_ARM_tensors_write_array(tensor, coords, value, operands, TYPE) {                                       \
+#define _emu_GL_ARM_tensors_write_array(tensor, tensorData, coords, value, operands, TYPE) {                           \
   int64_t _emu_GL_ARM_tensors_offset = 0;                                                                              \
   bool _emu_GL_ARM_tensors_outOfBounds = false;                                                                        \
   for (int _emu_GL_ARM_tensors_i = 0; _emu_GL_ARM_tensors_i < coords.length(); ++_emu_GL_ARM_tensors_i) {              \
@@ -74,15 +74,15 @@ const int g_TensorOperandsOutOfBoundsValue = 0x2;
     }                                                                                                                  \
                                                                                                                        \
     if (!_emu_GL_ARM_tensors_outOfBounds) {                                                                            \
-      tensor.address.data[uint(_emu_GL_ARM_tensors_offset + _emu_GL_ARM_tensors_i)] =                                  \
+      tensorData.data[uint(_emu_GL_ARM_tensors_offset + _emu_GL_ARM_tensors_i)] =                                      \
           TYPE(value[_emu_GL_ARM_tensors_i]);                                                                          \
     }                                                                                                                  \
   }                                                                                                                    \
 }
 
-#define _emu_GL_ARM_tensors_write_scalar(tensor, coords, value, operands, TYPE) {                                      \
+#define _emu_GL_ARM_tensors_write_scalar(tensor, tensorData, coords, value, operands, TYPE) {                          \
   TYPE _emu_GL_ARM_tensors_valueArr[] = {TYPE(value)};                                                                 \
-  _emu_GL_ARM_tensors_write_array(tensor, coords, _emu_GL_ARM_tensors_valueArr, operands, TYPE);                       \
+  _emu_GL_ARM_tensors_write_array(tensor, tensorData, coords, _emu_GL_ARM_tensors_valueArr, operands, TYPE);           \
 }
 
 )"
