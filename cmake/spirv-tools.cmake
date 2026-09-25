c74f1f953296b6308a66b67bf5d67aa9b8b5411e
hugookeeffe <hugo.okeeffe@arm.com>
2026-09-25 13:51:48 +0100

    chore(build): prioritize local SPIRV-Tools headers on Darwin
    Change-Id: I09c39b3bef15aad6c020ad5afe89a5d9abc6cddf
    Signed-off-by: hugookeeffe <hugo.okeeffe@arm.com>

--- cmake/spirv-tools.cmake
+++ cmake/spirv-tools.cmake
@@ -1,5 +1,5 @@
 #
-# SPDX-FileCopyrightText: Copyright 2024-2025 Arm Limited and/or its affiliates <open-source-office@arm.com>
+# SPDX-FileCopyrightText: Copyright 2024-2026 Arm Limited and/or its affiliates <open-source-office@arm.com>
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -13,7 +13,28 @@ if(EXISTS ${SPIRV_TOOLS_PATH}/CMakeLists.txt)
         option(SPIRV_SKIP_TESTS "" ON)
         option(SPIRV_WERROR "" OFF)
 
-        add_subdirectory(${SPIRV_TOOLS_PATH} spirv-tools SYSTEM EXCLUDE_FROM_ALL)
+        if(APPLE AND CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+            add_subdirectory(${SPIRV_TOOLS_PATH} spirv-tools EXCLUDE_FROM_ALL)
+        else()
+            add_subdirectory(${SPIRV_TOOLS_PATH} spirv-tools SYSTEM EXCLUDE_FROM_ALL)
+        endif()
+    endif()
+
+    # AppleClang searches /usr/local/include before command-line system include
+    # paths. Use normal include ordering for the source checkout while retaining
+    # system-header diagnostics for SPIR-V Tools headers.
+    if(APPLE AND CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+        foreach(SPIRV_TOOLS_INTERNAL_TARGET IN ITEMS
+                SPIRV-Tools SPIRV-Tools-static SPIRV-Tools-shared SPIRV-Tools-opt)
+            if(TARGET "${SPIRV_TOOLS_INTERNAL_TARGET}")
+                get_target_property(SPIRV_TOOLS_ALIASED_TARGET
+                    "${SPIRV_TOOLS_INTERNAL_TARGET}" ALIASED_TARGET)
+                if(NOT SPIRV_TOOLS_ALIASED_TARGET)
+                    target_compile_options("${SPIRV_TOOLS_INTERNAL_TARGET}" INTERFACE
+                        "$<$<COMPILE_LANGUAGE:CXX>:--system-header-prefix=spirv-tools/>")
+                endif()
+            endif()
+        endforeach()
     endif()
 
     mlsdk_get_git_revision(${SPIRV_TOOLS_PATH} SPIRV-Tools_VERSION)
